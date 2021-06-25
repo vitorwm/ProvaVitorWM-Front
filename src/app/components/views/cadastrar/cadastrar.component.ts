022d257c1791d19102c7036e989757b351492e6d
vitorwm <vitorwmartin@gmail.com>
2021-06-25 12:13:34 -0300

    Atualização do cadastrar que agora permite adicionar novos livros

--- src/app/components/views/cadastrar/cadastrar.component.ts
+++ src/app/components/views/cadastrar/cadastrar.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { LivroService } from "./../../../services/livro.service";
+import { Livro } from "../../../models/Livro";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
 
 @Component({
-  selector: 'app-cadastrar',
-  templateUrl: './cadastrar.component.html',
-  styleUrls: ['./cadastrar.component.css']
+  selector: "app-cadastrar",
+  templateUrl: "./cadastrar.component.html",
+  styleUrls: ["./cadastrar.component.css"],
 })
 export class CadastrarComponent implements OnInit {
+ 
+  nome!: string;
+  isbn!: string;
+  preco!: string;
+  data!: string;
 
-  constructor() { }
+  constructor(private service: LivroService, private router: Router, private snack: MatSnackBar) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  cadastrar(): void {
+    let livro = new Livro();
+    livro.nome = this.nome;
+    livro.isbn = this.isbn;
+
+    livro.preco = Number.parseInt(this.preco);
+
+
+    
+    this.service.cadastrar(livro).subscribe(ciclo => {
+      console.log(livro);
+      this.snack.open("Novo livro cadastrado com sucesso!", "", {
+        duration: 5000,
+        horizontalPosition: "right",
+        verticalPosition: "top",
+      });
+      this.router.navigate([""]);
+    });
   }
 
 }
